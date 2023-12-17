eafe0931e8d8821547b9695c48961b2b4e5714aa
Vicente <busslina@gmail.com>
2023-12-17 09:38:57 +0100

    initial commit

---
+++ bin/rearch_dart_test.dart
@@ -0,0 +1,5 @@
+import 'package:rearch_dart_test/rearch_dart_test.dart' as rearch_dart_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rearch_dart_test.calculate()}!');
+}
---
+++ test/rearch_dart_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:rearch_dart_test/rearch_dart_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
